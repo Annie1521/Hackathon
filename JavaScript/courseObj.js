01eed9554d94a69b08678c432e2e6972bf6a57da
unknown <alisong@lindenschool.ca>
2022-01-22 16:26:13 -0500

    finish grade 12 object part

--- JavaScript/courseObj.js
+++ JavaScript/courseObj.js
@@ -1,23 +1,122 @@
 courseInfoObj = {
     grade12: {
-        english: {
+        English: {
             ENG4U: {
+                simpleName: 'English',
                 sections: ['Literary Criticism', 'Poetry', 'Essay Analysis', 'Novel Study'],
                 summativeType: 'ISU'
             },
             ETS4U: {
+                simpleName: 'Literature',
                 sections: ['Apocalypse - Biblical Beginnings', 'Post Apocalypse', 'Sci-Fi Dystopia', 'Utopia', 'Speculative Fiction'],
                 summativeType: 'ISU'
             },
             EWC4U: {
+                simpleName: 'Writers Craft',
                 sections: ['Tools of Writing', 'Narrative Writing', 'Poetry', 'Informal Writing', 'Summative'],
-                summativeType: []
+                summativeType: ''
             }
         },
-        french: {
+        French: {
             FSF4U: {
+                simpleName: 'French',
+                sections: ['French Speaking World', 'Social Media', 'Art and Culture', 'Careers'],
+                summativeType: 'Exam'
+            }
+        },
+        Mathematics: {
+            MCV4U: {
+                simpleName: 'Calculus',
+                sections: ['Derivatives', 'Vectors', 'Planes', 'Exponential Functions'],
+                summativeType: 'Exam'
+            },
+            MHF4U: {
+                simpleName: 'Functions',
+                sections: ['Polynomial Functions', 'Polynomial Inequalities', 'Trigonometric Functions', 'Exponential and Logarithmic Functions', 'Combining Functions'],
+                summativeType: 'Exam'
+            },
+            MDM4U: {
+                simpleName: 'Data Management',
+                sections: ['Probability and Counting', 'Probability Distributions', 'Organizing Data for Analysis', 'Statistical Analysis'],
+                summativeType: 'Exam'
+            }
+        },
+        Science: {
+            SBI4U: {
+                simpleName: 'Biology',
+                sections: ['Biochemistry', 'Metabolic Processes', 'Molecular Genetics', 'Homeostasis', 'Population Dynamics'],
+                summativeType: 'Exam'
+            },
+            SCH4U: {
+                simpleName: 'Chemistry',
+                sections: ['Organic Chemistry', 'Structure and Properties of Matter', 'Energy Changes and Rate of Reaction', 'Chemical Systems and Equilibrium', 'Electrochemistry'],
+                summativeType: 'Exam'
+            },
+            SPH4U: {
+                simpleName: 'Physics',
+                sections: ['Dynamics', 'Energy and Momentum', 'Electric, Gravitational and Magnetic Feilds', 'Wave Nature of Light', 'Modern Physics'],
+                summativeType: 'Exam'
+            }
+        },
+        Arts: {
+            ADA4M: {
+                simpleName: 'Dramatic Arts',
+                sections: ['Techniques and Concentions', 'Real Life Drama', 'Character Delopement', 'Theatre History', 'Culture'],
+                summativeType: ''
+            },
+            AVI4M: {
+                simpleName: 'Visual Arts',
+                sections: ['Personal Portfolio', 'Collections in Visual Art', 'Spaces in Visual Art', 'Community Through Visual Art', 'Social Change Through Visual Art'],
+                summativeType: ''
+            },
+            AMS4U: {
+                simpleName: 'Music',
+                sections: ['Knowledge of String Intrument', 'Ability to Play String Instrument'],
+                summativeType: ''
+            },
+            AWQ4M: {
+                simpleName: 'Photography',
+                sections: ['Visual Storytelling', 'Self-Portraiture', 'The Photograph', 'The Photographer and the Creative Process'],
+                summativeType: ''
+            }
+        },
+        'Social Studies': {
+            HSB4U: {
+                simpleName: 'Challenge and Change',
+                sections: ['Social Change', 'Social Patterns and Trends', 'Global Social Change'],
+                summativeType: 'Exam'
+            },
+            HZT4U: {
+                simpleName: 'Philosophy',
+                sections: ['Philosophical Foundations', 'Metaphysics', 'Ethics', 'Political and Social Philosophy'],
+                summativeType: ''
+            }
+        },
+        Languages: {
+            LVV4U: {
+                simpleName: 'Classical Civilization',
+                sections: ['Prehistoric and Bronze Age Greece', 'Archaic Greece', 'Classical Greece', 'Roman Republic', 'Roman Empire'],
+                summativeType: ''
+            }
+        },
+        'Health and Physical Education': {
+            PPL4O: {
+                simpleName: 'Health and Physical Education',
+                sections: ['Fitness Plan', 'Confict Type and Resolution', 'Movement', 'Human Devolopment/Sexual Health', 'Fair Play', 'Healthy Eating'],
+                summativeType: ''
+            }
+        },
+        Technology: {
+            TGJ4M: {
+                simpleName: 'Communications',
+                sections: ['ICT Fundementals', 'Illustrated Print Fundemetials', 'Video Production', 'Blogging', 'Broadcast Jounalism'],
+                summativeType: ''
+            },
+            ICS4U: {
+                simpleName: 'Computer and Information Science',
+                sections: ['Java Programming', 'Arrays and Game Design', 'Software Life Cycle'],
+                summativeType: ''
             }
         }
     }
-
 }
