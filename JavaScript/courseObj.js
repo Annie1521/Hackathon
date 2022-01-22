d834ee554e56f91fd1c7797ab621ae4da5e1cd4a
unknown <alisong@lindenschool.ca>
2022-01-22 15:22:58 -0500

    adjusted some files and started obj

---
+++ JavaScript/courseObj.js
@@ -0,0 +1,23 @@
+courseInfoObj = {
+    grade12: {
+        english: {
+            ENG4U: {
+                sections: ['Literary Criticism', 'Poetry', 'Essay Analysis', 'Novel Study'],
+                summativeType: 'ISU'
+            },
+            ETS4U: {
+                sections: ['Apocalypse - Biblical Beginnings', 'Post Apocalypse', 'Sci-Fi Dystopia', 'Utopia', 'Speculative Fiction'],
+                summativeType: 'ISU'
+            },
+            EWC4U: {
+                sections: ['Tools of Writing', 'Narrative Writing', 'Poetry', 'Informal Writing', 'Summative'],
+                summativeType: []
+            }
+        },
+        french: {
+            FSF4U: {
+            }
+        }
+    }
+
+}
